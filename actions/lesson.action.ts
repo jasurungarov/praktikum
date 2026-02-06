3d4463f7ab77966b8238fcbecb19663218960309
jasurungarov <muhammadaliungarov@gmail.com>
2026-02-06 19:01:04 +0600

    managing lesson

--- actions/lesson.action.ts
+++ actions/lesson.action.ts
@@ -1,11 +1,19 @@
 'use server'
 
 import { connectToDatabase } from '@/lib/mongoose'
-import { ICreateLesson } from './types'
+import { ICreateLesson, ILessonFields, IUpdatePosition } from './types'
 import Section from '@/database/section.model'
 import Lesson from '@/database/lesson.model'
 import { revalidatePath } from 'next/cache'
 
+export const getLessons = async (section: string ) => {
+  try {
+    await connectToDatabase()
+    return await Lesson.find({ section }).sort({position: 1})
+  } catch (error) {
+    throw new Error('Somethinf went wrong!')
+  }
+}
 export const createLesson = async (params: ICreateLesson) => {
   try {
     await connectToDatabase()
@@ -44,7 +52,55 @@ export const createLesson = async (params: ICreateLesson) => {
 
     return { success: true }
   } catch (error) {
-    console.log('CREATE LESSON ERROR:', error)
-    throw error
+    throw new Error('Something went erong!')
   }
+}
+
+
+export const deleteLesson = async (id: string, path: string) => {
+	try {
+		await connectToDatabase()
+		const lesson = await Lesson.findById(id)
+		const section = await Section.findById(lesson.section)
+		section.lessons.pull(id)
+		section.save()
+		await Lesson.findByIdAndDelete(id)
+		revalidatePath(path)
+	} catch (error) {
+		throw new Error('Something went wrong!')
+	}
+}
+
+export const editLesson = async (
+	lesson: ILessonFields,
+	lessonId: string,
+	path: string
+) => {
+	try {
+		await connectToDatabase()
+		const duration = {
+			hours: Number(lesson.hours),
+			minutes: Number(lesson.minutes),
+			seconds: Number(lesson.seconds),
+		}
+
+		await Lesson.findByIdAndUpdate(lessonId, { ...lesson, duration })
+		revalidatePath(path)
+	} catch (error) {
+		throw new Error('Something went wrong!')
+	}
+}
+
+export const editLessonPosition = async (params: IUpdatePosition) => {
+	try {
+		await connectToDatabase()
+		const { lists, path } = params
+		for (const item of lists) {
+			await Lesson.findByIdAndUpdate(item._id, { position: item.position })
+		}
+
+		revalidatePath(path)
+	} catch (error) {
+		throw new Error('Something went wrong!')
+	}
 }
